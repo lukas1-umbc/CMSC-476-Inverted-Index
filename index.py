878fd078a0e6871560af9900194ffe79dd9ff064
Lukas Mueller <lukas1@umbc.edu>
2020-04-02 22:25:32 -0400

    createPostings() has been changed to createIndex() to more accurately reflect its function. It now outputs two files. postings.txt contains the name of each document that each token occurs in, and the weight of the token for each of those documents. index.txt contains each token, the number of documents that contain that token, and the line number of the first record for that token in postings.txt.

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ import time
 import math
 import re
 
-def tokenizeWords(text, filename, out_path, index, stop_set, file_lengths):
+def tokenizeWords(text, filename, out_path, postings_dict, stop_set, file_lengths):
 
     #statistics
     NUM_TOKENS = 0
@@ -24,70 +24,93 @@ def tokenizeWords(text, filename, out_path, index, stop_set, file_lengths):
             #don't want stop words or empty strings
             if good_word not in stop_set and good_word != "":
 
-                if good_word in index:
+                if good_word in postings_dict:
 
                     found_file = False
                     #look for current file in word's dictionary
-                    for fname in index[good_word]:
+                    for fname in postings_dict[good_word]:
 
                         #found
                         if fname == filename:
-                            index[good_word][fname] += 1
+                            postings_dict[good_word][fname] += 1
                             found_file = True
                             break
 
                     #add new entry to the word's dictionary if it didn't exist
                     if found_file == False:
-                        index[good_word][filename] = 1
-                #word was not in the index
+                        postings_dict[good_word][filename] = 1
+                #word was not in the postings_dict
                 else:
-                    index[good_word] = {}
-                    index[good_word][filename] = 1
+                    postings_dict[good_word] = {}
+                    postings_dict[good_word][filename] = 1
                     NUM_TOKENS += 1
 
     file_lengths[filename] = len(words)
     return NUM_TOKENS
 
 #return dictionary of all the tokens and their total frequencies
-def getTotalFreqs(index):
+def getTotalFreqs(postings_dict):
 
-    total_index = {}
-    for token, freq_dict in index.items():
+    total_postings_dict = {}
+    for token, freq_dict in postings_dict.items():
         freq_total = 0
         for filename in freq_dict:
             freq_total += freq_dict[filename]
-        total_index[token] = freq_total
-    return total_index
+        total_postings_dict[token] = freq_total
+    return total_postings_dict
 
 #remove all tokens whose frequency is <= the given threshold
-def removeLowFreq(index, threshold):
+def removeLowFreq(postings_dict, threshold):
 
-    total_index = getTotalFreqs(index)
-    for token, freq in total_index.items():
+    total_postings_dict = getTotalFreqs(postings_dict)
+    for token, freq in total_postings_dict.items():
         if freq <= threshold:
-            del index[token]
-    return index
+            del postings_dict[token]
+    return postings_dict
 
 #create postings dictionary, outputs postings to a file
-def createPostings(out_path, index, file_lengths):
+def createIndex(out_path, postings_dict, file_lengths):
+
+    #counter for the line being written to the postings file
+    line_num = 0
+
+    #sort postings_dict by token
+    postings_dict = dict(sorted(postings_dict.items()))
+
+    index = {}
+    #index will have tokens as keys, and a list containing the number of documents
+    #containing the token and the location of the first record for the token in the postings file
+    index = index.fromkeys(postings_dict.keys(), [0, 0])
 
     postings_file = open(out_path + "postings.txt", "w+")
+    index_file = open(out_path + "index.txt", "w+")
 
-    for token in index:
+    for token in postings_dict:
 
         #set the value of each document in the token's dictionary to the token's tfidf for that document
-        for filename in index[token]:
+        for filename in postings_dict[token]:
 
             #tf*idf = (tf of word in document/length of document) * log(number of documents/number of docs containting word)
-            tfidf = (index[token][filename]) * math.log(len(file_lengths)/len(index[token]))
+            tfidf = (postings_dict[token][filename]) * math.log(len(file_lengths)/len(postings_dict[token]))
             tfidf = round(tfidf, 5)
-            index[token][filename] = tfidf
+            postings_dict[token][filename] = tfidf
+
+        #in the index, set the number of documents the token occurs in
+        index[token][0] = len(postings_dict[token])
+
+        #in the index, set the location of the first record for the token in the postings file 
+        index[token][1] = line_num
 
         #write the filename and weight for that file of each token to postings file
-        for filename, weight in index[token].items():
+        for filename, weight in postings_dict[token].items():
             postings_file.write(filename + ", " + str(weight) + "\n")
+            line_num += 1
+
+        #write the index dictionary contents to the index file
+        index_file.write(token + "\n" + str(index[token][0]) + "\n" + str(index[token][1]) + "\n")
 
     postings_file.close()
+    index_file.close()
 
 def main(argv):
 
@@ -120,7 +143,7 @@ def main(argv):
     stop_file.close()
 
     #dictionary for the tokens and their frequencies for each file
-    index = {}
+    postings_dict = {}
     #dictionary for each file's length
     file_lengths = {}
 
@@ -131,7 +154,7 @@ def main(argv):
         #strip html
         text = text_maker.handle(html_file.read())
 
-        NUM_TOKENS += tokenizeWords(text, filename, out_path, index, stop_set, file_lengths)
+        NUM_TOKENS += tokenizeWords(text, filename, out_path, postings_dict, stop_set, file_lengths)
 
         html_file.close()
 
@@ -140,9 +163,9 @@ def main(argv):
 
     #throw out low frequency tokens
     threshold = 1
-    index = removeLowFreq(index, threshold)
+    postings_dict = removeLowFreq(postings_dict, threshold)
 
-    createPostings(out_path, index, file_lengths)
+    createIndex(out_path, postings_dict, file_lengths)
     
     print(str(NUM_TOKENS) + " distinct tokens found.")
 
